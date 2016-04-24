8a0cc9c461d04e51729885e6ba70713072ae94e4
Neo Wei <neowei03@gmail.com>
2016-04-24 17:00:24 +0800

    home page

--- www/js/native.js
+++ www/js/native.js
@@ -1,17 +1,34 @@
 var jqueryReady = $.Deferred();
 var cordovaReady = $.Deferred();
 
-$(function() {
-
+$(function () {
     jqueryReady.resolve();
-
 });
 
-document.addEventListener("deviceready", function() {
+document.addEventListener("deviceready", function () {
     cordovaReady.resolve();
+    alert("Yes Device ready!");//todo yg
 }, false);
 
-$.when(jqueryReady, cordovaReady).done(function() {
-    }).error(function() {
-    alert("Error");//todo yg
+$.when(jqueryReady, cordovaReady).done(function () {
+    alert("All ready");
+}).error(function () {
+    alert("Error");
 });
+
+
+function takePic() {
+    alert("hi");
+    var tapEnabled = true; //enable tap take picture
+    var dragEnabled = true; //enable preview box drag across the screen
+    var toBack = true; //send preview box to the back of the webview
+    var rect = {x: 100, y: 100, width: 200, height: 200};
+    cordova.plugins.camerapreview.startCamera(rect, "front", tapEnabled, dragEnabled, toBack);
+    alert("hi1");
+    cordova.plugins.camerapreview.show();
+    alert("hi2");
+    cordova.plugins.camerapreview.takePicture({maxWidth: 640, maxHeight: 640});
+    alert("hi3");
+    cordova.plugins.camerapreview.hide();
+    alert("bye");
+}
--- www/js/rentbuddy.js
+++ www/js/rentbuddy.js
@@ -7,4 +7,7 @@ var myApp = new Framework7({
 var $$ = Dom7;
 
 // Add main view
-var mainView = myApp.addView('.view-main', {});
+var mainView = myApp.addView('.view-main', {
+    // Enable Dynamic Navbar for this view
+    dynamicNavbar: true,
+});
