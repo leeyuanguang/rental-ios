7968a36dd4514fd9cbe143fa8c4d7ed5a9b09079
Neo Wei <neowei03@gmail.com>
2016-05-01 18:26:51 +0800

    update ui

--- www/js/shared-functions.js
+++ www/js/shared-functions.js
@@ -3,3 +3,7 @@ function setTwoNumberDecimal() {
     var value = document.getElementById("itemPricePerDay").value;
     document.getElementById("itemPricePerDay").value = parseFloat(value).toFixed(2);
 }
+
+$('input, textarea, button, a, select').off('touchstart mousedown').on('touchstart mousedown', function(e) {
+    e.stopPropagation();
+});
