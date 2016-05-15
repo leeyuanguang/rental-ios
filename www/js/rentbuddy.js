20b635ec896477facac9f2414e775816d83b0f3a
Neo Wei <neowei03@gmail.com>
2016-05-15 18:56:48 +0800

    added aviary

--- www/js/rentbuddy.js
+++ www/js/rentbuddy.js
@@ -18,6 +18,8 @@ var $$ = Dom7;
 var mainView = myApp.addView('.view-main', {
 });
 
+var featherEditor;
+
 myApp.onPageInit('create-item', function (page) {
     var pickerCategory = myApp.picker({
         input: '#picker-category',
@@ -27,6 +29,22 @@ myApp.onPageInit('create-item', function (page) {
                 values: ['Costumes', 'Electronics', 'Photography', 'Sports', 'Others']
             }
         ]
+    });
+
+    //Instantiate Feather
+    featherEditor = new Aviary.Feather({
+        apiKey: '2f7713fd8a924fa59ba103abc91e7ee6',
+        theme: 'light', // Check out our new 'light' and 'dark' themes!
+        tools: 'all',
+        appendTo: '',
+        cropPresets: ['Square', '1:1'],
+        onSave: function (imageID, newURL) {
+            var img = document.getElementById(imageID);
+            img.src = newURL;
+        },
+        onError: function (errorObj) {
+            alert(errorObj.message);
+        }
     });
 });
 
@@ -39,6 +57,14 @@ myApp.onPageInit('index', function (page) {
 //    mainView.router.load({url: 'signup-splash.html', reload: true, animatePages: true});//back to previous page     
 //     }
 }).trigger();
+
+function launchEditor(id, src) {
+    featherEditor.launch({
+        image: id,
+        url: src
+    });
+    return false;
+}
 
 function navigateToSearch() {
     console.log("navigateToSearch");
