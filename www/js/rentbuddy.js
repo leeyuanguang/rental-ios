def515442519e7254d5e3b820b258afa3fd3b5eb
Neo Wei <neowei03@gmail.com>
2016-06-04 11:24:43 +0800

    trying to add crop function

--- www/js/rentbuddy.js
+++ www/js/rentbuddy.js
@@ -48,5 +48,31 @@ function navigateToSearch() {
 }
 
 
-myApp.onPageBeforeAnimation('message-view', function (page) {
+myApp.onPageAfterAnimation('crop', function (page) {
+    console.log('1');
+
+    $(function () {
+        $('#yourimage').each(function () {
+            var image = $(this),
+                    cropwidth = image.attr('cropwidth'),
+                    cropheight = image.attr('cropheight'),
+                    results = image.next('.results'),
+                    x = $('.cropX', results),
+                    y = $('.cropY', results),
+                    w = $('.cropW', results),
+                    h = $('.cropH', results),
+                    download = results.next('.download').find('a');
+
+            image.cropbox({width: cropwidth, height: cropheight, showControls: 'auto'})
+                    .on('cropbox', function (event, results, img) {
+                        x.text(results.cropX);
+                        y.text(results.cropY);
+                        w.text(results.cropW);
+                        h.text(results.cropH);
+                        download.attr('href', img.getDataURL());
+                    });
+        });
+    });
+
+    console.log('2');
 });
